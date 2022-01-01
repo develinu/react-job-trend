9b1cbdff1ae0d093ab08b1ddf820867c98ece140
develinu <iwlee.dev@gmail.com>
2022-01-02 02:52:43 +0900

    feat: 헤더 및 직업 정보 데이터 추가

---
+++ src/data/header.js
@@ -0,0 +1,15 @@
+export const logo = {
+  src: "/logo192.png",
+  alt: "logo192.png",
+}
+
+export const menus = [
+  {
+    id: 1,
+    name: "직무 트렌드"
+  },
+  {
+    id: 2,
+    name: "예정중..."
+  }
+]
