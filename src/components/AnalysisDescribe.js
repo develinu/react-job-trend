87ef50bdc4bad244db84524673e0699bfe02bc17
develinu <iwlee.dev@gmail.com>
2022-01-02 02:51:37 +0900

    feat: 직무 설명 컴포넌트 추가

---
+++ src/components/AnalysisDescribe.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './AnalysisDescribe.scss'
+import Tags from './Tags'
+
+
+const AnalysisDescribe = () => {
+  return (
+    <div className="analysis-describe">
+      <p>다음 34개 채용공고에 대한 분석 결과입니다.</p>
+      <Tags />
+    </div>
+  )
+}
+
+export default AnalysisDescribe
