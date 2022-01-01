d309774feb35a6930d9bc74ec68bf7afe19d6ad1
develinu <iwlee.dev@gmail.com>
2022-01-02 02:52:26 +0900

    feat: 태그 리스트 컴포넌트 추가

---
+++ src/components/Tags.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import './Tags.scss'
+
+
+const Tags = () => {
+  return (
+    <div className="tags">
+      <Tag id={1} name={"tag1"}/>
+      <Tag id={2} name={"tag2"}/>
+      <Tag id={3} name={"tag3"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+      <Tag id={4} name={"tag4123"}/>
+    </div>
+  )
+}
+
+const Tag = ({ id, name }) => {
+  return (
+    <div className="tag">
+      {name}
+    </div>
+  )
+}
+
+export default Tags
