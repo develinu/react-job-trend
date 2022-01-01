7cffd64926ef3d9d0f1423ec9925261ddc4083e6
develinu <iwlee.dev@gmail.com>
2022-01-02 02:49:31 +0900

    feat: App 컴포넌트 작성 및 의존성 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.scss';
+import GlobalStyles from './GlobalStyles'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Nav from './components/Nav'
+import DateDropdown from './components/DateDropdown';
+import Tab from './components/Tab'
+import AnalysisDescribe from './components/AnalysisDescribe';
+import AnalysisCharts from './components/AnalysisCharts';
+
+import { jobs } from './data/JobInfo'
+
 
 function App() {
   return (
+    <>
+    <GlobalStyles />
     <div className="App">
-    </div>
+      <header>
+        <Nav />
+      </header>
+      <div className="container">
+        <section className="job-filter">
+          <DateDropdown />
+          <Tab jobs={jobs} />
+        </section>
+        <section className="job-intro">
+          <AnalysisDescribe />
+        </section>
+        <section className="charts">
+          <AnalysisCharts />
+        </section>
+      </div>
+    </div>    
+    </>
   );
 }
 
